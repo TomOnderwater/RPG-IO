a67abf7f80f125e62ca87a5f9d0d02067c531b1f
TomOnderwater <t.c.onderwater@gmail.com>
2022-08-26 13:07:21 +0200

    changed port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 let path = require("path")
-const port = 3000
+const port = 80
 
 const expressWS = require('express-ws')(app)
 const bodyParser = require("body-parser")
